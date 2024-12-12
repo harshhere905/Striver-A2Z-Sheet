c1558f5636b52386c0248776154dc8c92de2e0ad
Priyanshu Varshney <hv96829@gmail.com>
2024-12-12 23:03:57 +0530

    Time: 3 ms (53.88%), Space: 15 MB (61.67%) - LeetHub

---
+++ 1741-sort-array-by-increasing-frequency/1741-sort-array-by-increasing-frequency.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> frequencySort(vector<int>& nums) {
+       unordered_map<int,int>mp;
+       for(int n:nums){
+        mp[n]++;
+       }
+       sort(nums.begin(),nums.end(),[&](int a,int b){
+        return mp[a]!=mp[b]?mp[a]<mp[b]:a>b;});
+        return nums;
+    }
+};
