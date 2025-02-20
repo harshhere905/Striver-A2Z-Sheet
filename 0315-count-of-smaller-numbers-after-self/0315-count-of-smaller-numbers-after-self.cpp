35e212823708641a5c40711d3f5699662c5cd070
Priyanshu Varshney <hv96829@gmail.com>
2025-02-20 23:09:40 +0530

    Time: 1731 ms (6.35%), Space: 96.4 MB (83.26%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.cpp
@@ -5,11 +5,10 @@ public:
         vector<int> sortedArr;
         
         for (int i = nums.size() - 1; i >= 0; i--) {
-            int idx = lower_bound(sortedArr.begin(), sortedArr.end(), nums[i]) - sortedArr.begin();
+            int idx = lower_bound(sortedArr.begin(), sortedArr.end(), nums[i]) - sortedArr.begin();                                            
             ans.push_back(idx);
             sortedArr.insert(sortedArr.begin() + idx, nums[i]); 
         }
-        
         reverse(ans.begin(), ans.end());
         return ans;
     }
