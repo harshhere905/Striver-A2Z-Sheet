f9001855d86ddfc302610b8331c93a12a4aa2998
Priyanshu Varshney <hv96829@gmail.com>
2025-03-04 01:46:25 +0530

    Time: 13 ms (78.33%), Space: 50.1 MB (11.48%) - LeetHub

--- 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
+++ 0943-sum-of-subarray-minimums/0943-sum-of-subarray-minimums.cpp
@@ -1,31 +1,49 @@
 class Solution {
 public:
+    vector<int>get_left(vector<int>arr,int n){
+         vector<int>result(n);
+         stack<int>st;
+         for(int i=0;i<n;i++){
+            if(st.empty()){
+                result[i]=-1;
+            }
+            while(!st.empty()&&arr[st.top()]>arr[i]){
+                st.pop();
+            }
+            result[i]=st.empty()?-1:st.top();
+            st.push(i);
+         }
+         return result;
+    }
+     vector<int>get_right(vector<int>arr,int n){
+         vector<int>result(n);
+         stack<int>st;
+         for(int i=n-1;i>=0;i--){
+            if(st.empty()){
+                result[i]=n;
+            }
+            while(!st.empty()&&arr[st.top()]>=arr[i]){
+                st.pop();
+            }
+            result[i]=st.empty()?n:st.top();
+            st.push(i);
+         }
+         return result;
+    }
     int sumSubarrayMins(vector<int>& arr) {
-        long long int ans=0;
-        for(int i=0;i<arr.size();i++){
-            int l=0;
-            int r=0;
-            for(int j=i-1;j>=0;j--){
-               if(arr[j]>arr[i]){
-                 l++;
-               }
-               else{
-                break;
-               }
-            }
-            for(int j=i+1;j<arr.size();j++){
-               if(arr[j]>=arr[i]){
-                 r++;
-               }
-               else{
-                break;
-               }
-            }
-            long long int res=(l+1)*(r+1);
-            ans+=arr[i]*res;
+        int n=arr.size();
+        vector<int>right_smaller=get_right(arr,n);
+        vector<int>left_smaller=get_left(arr,n);
+        long long sum=0;
+        int M=1e9+7;
+        for(int i=0;i<n;i++){
+            long long d1=i-left_smaller[i];
+            long long d2=right_smaller[i]-i;
+
+            long long total=d1*d2;
+            long long res=total*arr[i];
+            sum=(sum+res)%M;
         }
-        const int MOD = 1e9 + 7;
-        long long int res=(ans)%MOD;
-        return res;
+        return sum;
     }
 };
