eeb88e32aa480368b5a891d06532e946dd90e089
Priyanshu Varshney <hv96829@gmail.com>
2025-03-07 00:57:59 +0530

    Time: 25 ms (96.48%), Space: 13 MB (84.88%) - LeetHub

--- 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
+++ 0516-longest-palindromic-subsequence/0516-longest-palindromic-subsequence.cpp
@@ -18,9 +18,9 @@ public:
         return t[i][j]= max(find_palindrome(s,i+1,j),find_palindrome(s,i,j-1));
     }
     int longestPalindromeSubseq(string s) {
-        if(s.size()==1){
-            return 1;
-        }
+        // if(s.size()==1){
+        //     return 1;
+        // }
         memset(t,-1,sizeof(t));
         n=s.size();
         return find_palindrome(s,0,n-1);
