50d4329726e6e963a229a05fb334d44f1417f94f
Priyanshu Varshney <hv96829@gmail.com>
2025-03-07 12:01:07 +0530

    Time: 374 ms (27.03%), Space: 17.5 MB (56.3%) - LeetHub

--- 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
+++ 2610-closest-prime-numbers-in-range/2610-closest-prime-numbers-in-range.cpp
@@ -1,13 +1,12 @@
 class Solution {
 public:
     int is_prime(int n){
-        if(n==1){
+        if (n < 2) return false;
+        if (n == 2 || n == 3) return true;
+        if (n % 2 == 0 || n % 3 == 0) return false;
+        for (int i = 5; i * i <= n; i += 6) {
+        if (n % i == 0 || n % (i + 2) == 0)
             return false;
-        }
-        for(int i=2;i*i<=n;i++){
-            if(n%i==0){
-                return false;
-            }
         }
         return true;
     }
