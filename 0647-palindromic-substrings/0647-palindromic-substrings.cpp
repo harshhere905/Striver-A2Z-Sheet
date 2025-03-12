86156fc9e1e7dba2eda779f9f9dcf438e5efdb8b
Priyanshu Varshney <hv96829@gmail.com>
2025-03-13 00:27:05 +0530

    Time: 35 ms (23.94%), Space: 12.9 MB (44.05%) - LeetHub

--- 0647-palindromic-substrings/0647-palindromic-substrings.cpp
+++ 0647-palindromic-substrings/0647-palindromic-substrings.cpp
@@ -5,6 +5,9 @@ class Solution {
         if(i>=j){
             return true;
         }
+        if(t[i][j]!=-1){
+            return t[i][j];
+        }
         if(s[i]==s[j]){
             return t[i][j]= solve(s,i+1,j-1);
         }
