b31d84f813e891961a070d70dfb564d2dd5dcc6a
Priyanshu Varshney <hv96829@gmail.com>
2025-03-15 01:22:59 +0530

    Time: 2139 ms (5.01%), Space: 526.9 MB (5%) - LeetHub

--- 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
+++ 3643-zero-array-transformation-ii/3643-zero-array-transformation-ii.cpp
@@ -1,26 +1,51 @@
 class Solution {
 public:
-    static int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
-        const int n=nums.size(), m=queries.size();
-        vector<int> freq(n+1);
-        int op=0, k=0;
-        for(int i=0; i<n; i++){
-            for( ; op < nums[i]-freq[i]; k++){
-                if (k>=m) return -1;
-                const int l=queries[k][0], r=queries[k][1], v=queries[k][2];
-                if (r<i) continue;
-                freq[max(l, i)]+=v;
-                freq[r+1]-=v;
+    int n;
+    int Q;
+    bool check(vector<int>nums,vector<vector<int>>queries,int mid){
+           vector<int>diff(n,0);
+           for(int i=0;i<=mid;i++){
+              int l=queries[i][0];
+              int r=queries[i][1];
+              int x=queries[i][2];
+              diff[l]+=x;
+              if(r+1<n){
+                diff[r+1]-=x;
+              }
+           }
+           int curr_sum=0;
+           for(int i=0;i<n;i++){
+            curr_sum+=diff[i];
+            diff[i]=curr_sum;
+            if(nums[i]-diff[i]>0){
+                return false;
             }
-            op+=freq[i];
+           }
+           return true;
+    }
+    int minZeroArray(vector<int>& nums, vector<vector<int>>& queries) {
+        n=nums.size();
+        Q=queries.size();
+        auto lambda= [](int x){
+            return x==0;
+        };
+        if(all_of(nums.begin(),nums.end(),lambda)){
+            return 0;
+        }
+        int l=0;
+        int r=Q-1;
+        int k=-1;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(check(nums,queries,mid)==true){
+                k=mid+1;
+                r=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+
         }
         return k;
     }
 };
-
-auto init = []() {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    return 'c';
-}();  
