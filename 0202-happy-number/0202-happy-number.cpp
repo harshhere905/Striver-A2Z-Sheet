da11a3427e4c5f132665f44cbba74bd13ecf082f
Priyanshu Varshney <hv96829@gmail.com>
2025-03-20 23:13:26 +0530

    Time: 0 ms (100%), Space: 7.7 MB (84.86%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool isHappy(int n) {
+        if(n==1 || n==7){
+            return true;
+        }
+       while(true){
+          int sum=0;
+           while(n>0){
+              int temp=n%10;
+              sum+=temp*temp;
+              n=n/10;
+           }
+           n=sum;
+           if(n==1 || n==7){
+            return true;
+           }
+           if(n%10==n && n!=1){
+              break;
+           }
+       }
+       return false;
+    }
+};
