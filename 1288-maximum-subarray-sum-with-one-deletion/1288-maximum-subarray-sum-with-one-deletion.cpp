8717816e802d6c137f5757c66ae782b595b2c500
Priyanshu Varshney <hv96829@gmail.com>
2025-03-21 11:41:10 +0530

    Time: 34 ms (28.28%), Space: 47.9 MB (5.16%) - LeetHub

--- 1288-maximum-subarray-sum-with-one-deletion/1288-maximum-subarray-sum-with-one-deletion.cpp
+++ 1288-maximum-subarray-sum-with-one-deletion/1288-maximum-subarray-sum-with-one-deletion.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-
-    int solve(vector<int>& nums, int i, int canDelete) {
-        if (i >= nums.size()) return INT_MIN;
-        if (dp[i][canDelete] != -1) return dp[i][canDelete];
-
-        int take = nums[i] + max(0, solve(nums, i + 1, canDelete));
-        int del = canDelete ? solve(nums, i + 1, 0) : INT_MIN;
-
-        return dp[i][canDelete] = max(take, del);
+    vector<vector<int>>dp;
+    int solve(vector<int>&res,int i,int check){
+        if(i>=res.size()){
+            return INT_MIN;
+        }
+        if(dp[i][check]!=-1){
+            return dp[i][check];
+        }
+        int take=res[i]+max(0,solve(res,i+1,check));
+        int del=check?solve(res,i+1,0):INT_MIN;
+        return dp[i][check]=max(take,del);
     }
-
     int maximumSum(vector<int>& arr) {
-        int n = arr.size();
-        dp.resize(n, vector<int>(2, -1));
-
-        int result = INT_MIN;
-        for (int i = 0; i < n; i++) {
-            result = max(result, solve(arr, i, 1));
+        dp.resize(arr.size(),vector<int>(2,-1));
+        int n=arr.size();
+        int result=INT_MIN;
+        for(int i=0;i<n;i++){
+            result=max(result,solve(arr,i,1));
         }
         return result;
     }
