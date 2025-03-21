570791152edbfe2251ff382c8f76774361636774
Priyanshu Varshney <hv96829@gmail.com>
2025-03-21 10:27:14 +0530

    Time: 0 ms (100%), Space: 7.7 MB (84.86%) - LeetHub

--- 0202-happy-number/0202-happy-number.cpp
+++ 0202-happy-number/0202-happy-number.cpp
@@ -15,7 +15,7 @@ public:
            if(n==1 || n==7){
             return true;
            }
-           if(n%10==n && n!=1){
+           if(n%10==n){
               return false;
            }
        }
