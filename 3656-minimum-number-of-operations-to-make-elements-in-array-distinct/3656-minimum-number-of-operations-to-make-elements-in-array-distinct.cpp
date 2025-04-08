4741f831166c77eaa486fb4cfe87835bdab8a773
Priyanshu Varshney <hv96829@gmail.com>
2025-04-08 20:48:27 +0530

    Time: 0 ms (100%), Space: 27.8 MB (54.27%) - LeetHub

--- 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
+++ 3656-minimum-number-of-operations-to-make-elements-in-array-distinct/3656-minimum-number-of-operations-to-make-elements-in-array-distinct.cpp
@@ -1,41 +1,12 @@
 class Solution {
 public:
-    int minimumOperations(vector<int>& nums) {
-        // if(nums.size()<3){
-        //     return 1;
-        // }
-        unordered_map<int,int>mp;
-        for(auto n:nums){
-            mp[n]++;
-        }
-        bool temp=true;
-        for(auto it:mp){
-            if(it.second>1){
-                temp=false;
+    int minimumOperations(std::vector<int>& nums) {
+        unordered_set<int> seen;
+        for (int i = nums.size() - 1; i >= 0; --i) {
+            if (!seen.insert(nums[i]).second) {
+                return i / 3 + 1;
             }
         }
-        if(temp){
-            return 0;
-        }
-        int cnt=1;
-        for(int i=0;i<nums.size();i+=3){
-            for(int j=i;j<i+3;j++){
-                if(j<nums.size()){
-                mp[nums[j]]--;
-                }
-            }
-            bool check=true;
-            for(auto it:mp){
-                if(it.second>1){
-                    check=false;
-                    cnt++;
-                    break;
-                }
-            }
-            if(check){
-                break;
-            }
-        }
-        return cnt;
+        return 0;
     }
 };
