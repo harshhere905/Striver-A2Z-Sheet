bacd1c93098346b3e30b1d0a66481d340367cb36
Priyanshu Varshney <hv96829@gmail.com>
2025-04-20 00:57:33 +0530

    Time: 7 ms (97.75%), Space: 80.8 MB (77.17%) - LeetHub

--- 2664-maximize-greatness-of-an-array/2664-maximize-greatness-of-an-array.cpp
+++ 2664-maximize-greatness-of-an-array/2664-maximize-greatness-of-an-array.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
     int maximizeGreatness(vector<int>& nums) {
-        int cnt=0;
         sort(nums.begin(),nums.end());
-        vector<int>temp(nums.size());
-        for(int i=0;i<nums.size();i++){
-            temp[i]=nums[i];
-        }
-        for(int i=0;i<nums.size();i++){
-            auto it=upper_bound(nums.begin()+i,nums.end(),temp[i]);
-            if(it!=nums.end()){
-                *it=-1;
-                cnt++;
-            }
+        int i=0;
+        int j=0;
+        int cnt=0;
+        int n=nums.size();
+        while(i<n && j<n){
+           if(nums[i]<nums[j]){
+              i++;
+              cnt++;
+           }
+           j++;
         }
         return cnt;
     }
