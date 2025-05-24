98ef765aa369e687c22e9284badccf39d6c5e383
Priyanshu Varshney <hv96829@gmail.com>
2025-05-24 13:54:56 +0530

    Time: 20 ms (36.73%), Space: 45.7 MB (68.35%) - LeetHub

---
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        sort(people.begin(),people.end());
+        int low=0;
+        int high=people.size()-1;
+        int cnt=0;
+        while(low<=high){
+            if(people[low]+people[high]<=limit){
+                low++;
+            }
+            high--;
+            cnt++;
+        }
+        return cnt;
+    }
+};
