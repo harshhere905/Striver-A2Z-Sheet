312c23deeefb8c1b7aac186f891780d924345bd1
Priyanshu Varshney <hv96829@gmail.com>
2025-06-19 18:37:06 +0530

    Time: 3 ms (10.81%), Space: 8.4 MB (30.58%) - LeetHub

---
+++ 0807-custom-sort-string/0807-custom-sort-string.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string customSortString(string order, string s) {
+        string ans="";
+        string temp="";
+        unordered_map<char,int>mp;
+        for(auto it:s){
+            mp[it]++;
+        }
+        for(auto it:order){
+            if(s.find(it)!=string::npos){
+               while(mp[it]--){
+               ans+=it;
+               }
+            }
+        }
+        for(auto it:s){
+            if(ans.find(it)==string::npos){
+                temp+=it;
+            }
+        }
+        ans+=temp;
+        return ans;
+    }
+};
