932190552afafb75576c93b357fdc1d5ee0bb58a
Priyanshu Varshney <hv96829@gmail.com>
2025-07-01 01:39:52 +0530

    Time: 28 ms (45.69%), Space: 43.9 MB (29.87%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -1,51 +1,17 @@
 class Solution {
 public:
-    int idx=-1;
-    int firstocc(int& midy,vector<int>& temp){
-        int index=-1;
-        int l=0;
-        int h=temp.size()-1;
-        while(l<=h){
-            int mid=(l+h)/2;
-            if(temp[mid]==midy){
-                h=mid-1;
-                index=mid;
-            }
-            else if(temp[mid]>midy){
-                h=mid-1;
-            }
-            else{
-                l=mid+1;
-            }
-        }
-        return index;
-    }
-    bool check(int& num,vector<int>& temp){
-        int low=0;
-        int high=temp.size()-1;
-        while(low<=high){
-            int mid=(low+high)/2;
-            if(num-temp[mid]==1){
-                idx=firstocc(temp[mid],temp);
-                return true;
-            }
-            else if(num-temp[mid]>1){
-                low=mid+1;
-            }
-            else{
-                high=mid-1;
-            }
-        }
-        return false;
-    }
     int findLHS(vector<int>& nums) {
-       sort(nums.begin(),nums.end());
-       int ans=0;
-       for(int j=nums.size()-1;j>=0;j--){
-           if(check(nums[j],nums)){
-              ans=max(ans,j-idx+1);
-           }
-       }
-       return ans; 
+        unordered_map<int, int> freq;
+        for (int num : nums) {
+            freq[num]++;
+        }
+
+        int ans = 0;
+        for (auto& [num, count] : freq) {
+            if (freq.find(num + 1) != freq.end()) {
+                ans = max(ans, count + freq[num + 1]);
+            }
+        }
+        return ans;
     }
 };
