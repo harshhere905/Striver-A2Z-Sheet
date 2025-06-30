cbb6703509afd89031814577c2495274b8c2717a
Priyanshu Varshney <hv96829@gmail.com>
2025-07-01 01:16:22 +0530

    Time: 29 ms (44.25%), Space: 36.1 MB (95.08%) - LeetHub

--- 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
+++ 0594-longest-harmonious-subsequence/0594-longest-harmonious-subsequence.cpp
@@ -1,15 +1,50 @@
 class Solution {
 public:
+    int idx=-1;
+    int firstocc(int& midy,vector<int>& temp){
+        int index=-1;
+        int l=0;
+        int h=temp.size()-1;
+        while(l<=h){
+            int mid=(l+h)/2;
+            if(temp[mid]==midy){
+                h=mid-1;
+                index=mid;
+            }
+            else if(temp[mid]>midy){
+                h=mid-1;
+            }
+            else{
+                l=mid+1;
+            }
+        }
+        return index;
+    }
+    bool check(int& num,vector<int>& temp){
+        int low=0;
+        int high=temp.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(num-temp[mid]==1){
+                idx=firstocc(temp[mid],temp);
+                return true;
+            }
+            else if(num-temp[mid]>1){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        return false;
+    }
     int findLHS(vector<int>& nums) {
        sort(nums.begin(),nums.end());
        int ans=0;
-       for(int i=0;i<nums.size();i++){
-          for(int j=nums.size()-1;j>=0;j--){
-              if(nums[j]-nums[i]==1){
-                 ans=max(ans,j-i+1);
-                 break;
-              }
-          }
+       for(int j=nums.size()-1;j>=0;j--){
+           if(check(nums[j],nums)){
+              ans=max(ans,j-idx+1);
+           }
        }
        return ans; 
     }
